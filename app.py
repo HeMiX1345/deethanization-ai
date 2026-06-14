d281f5b0cab29e7dba9b7cfbae2a809065b4b2d8
HeMiX135 <yasemenov2005@gmail.com>
2026-06-14 20:00:36 +0500

    Update app.py

--- app.py
+++ app.py
@@ -274,7 +274,7 @@ def render_scheme_with_overlay(
                         <div style="font-size:18px;font-weight:800;color:#245a2d;margin-bottom:8px;">{methane_val:.4f}</div>
                         <div style="font-size:11px;color:#555;">Содержание этана</div>
                         <div style="font-size:18px;font-weight:800;color:#245a2d;margin-bottom:8px;">{ethane_val:.4f}</div>
-                        <div style="font-size:10px;color:#666;">система измерения: масса в долях</div>
+                        <div style="font-size:10px;color:#666;">Массовые доли</div>
                     </div>
                 </div>
 
@@ -325,11 +325,12 @@ def render_scheme_with_overlay(
                     ">
                         <div style="font-size:14px;font-weight:700;color:#222;margin-bottom:10px;">K-301</div>
                         <div style="font-size:11px;color:#555;">Температура верха</div>
+                        <div style="font-size:11px;color:#555;">Температура низа</div>
                         <div style="font-size:18px;font-weight:800;color:#234c28;margin-bottom:6px;">{top_val:.2f} ℃</div>
                         <div style="font-size:11px;color:#555;">Горячая зона</div>
                         <div style="font-size:18px;font-weight:800;color:#234c28;margin-bottom:6px;">{hot_val:.2f} ℃</div>
                         <div style="font-size:11px;color:#555;">Холодная зона</div>
-                        <div style="font-size:18px;font-weight:800;color:#234c28;">{cold_val:.2f} </div>
+                        <div style="font-size:18px;font-weight:800;color:#234c28;">{cold_val:.2f} ℃</div>
                     </div>
 
                     <div style="
@@ -340,6 +341,7 @@ def render_scheme_with_overlay(
                     ">
                         <div style="font-size:14px;font-weight:700;color:#222;margin-bottom:6px;">Масса КГД</div>
                         <div style="font-size:22px;font-weight:800;color:#245a2d;">{kgd_val:.2f}</div>
+                        <div style="font-size:10px;color:#666;">тонн/час</div>
                     </div>
 
                     <div style="
@@ -351,7 +353,7 @@ def render_scheme_with_overlay(
                         <div style="font-size:14px;font-weight:700;color:#222;margin-bottom:6px;">Масса выхода</div>
                         <div style="font-size:11px;color:#555;">Вывод балансового избытка</div>
                         <div style="font-size:22px;font-weight:800;color:#245a2d;margin-bottom:4px;">{excess_val:.2f}</div>
-                        <div style="font-size:10px;color:#666;">система исчисления: тонн/час</div>
+                        <div style="font-size:10px;color:#666;">тонн/час</div>
                     </div>
                 </div>
             </div>
